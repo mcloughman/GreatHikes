8db223554cda294595d9dd0bb970c8b26904cea2
mcloughman <51391940+mcloughman@users.noreply.github.com>
2020-10-08 14:28:22 -0700

    set up all hikes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 const axios = require("axios");
 
 // APP CONFIG
@@ -12,65 +13,94 @@ mongoose
   })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
+mongoose.set("useFindAndModify", false);
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 // MONGOOSE MODEL CONFIG
-var birdSchema = new mongoose.Schema({
+var hikeSchema = new mongoose.Schema({
   name: String,
-  image: String,
+  image: [],
   description: String,
   created: { type: Date, default: Date.now },
 });
 
-var Bird = mongoose.model("Bird", birdSchema);
+var Hike = mongoose.model("Hike", hikeSchema);
 
 // ROUTES
 // INDEX
-app.get("/", () => {
-  res.redirect("/blogs");
+app.get("/", (req, res) => {
+  res.redirect("/hikes");
 });
 
-app.get("/birds", async (req, res) => {
+app.get("/hikes", async (req, res) => {
   try {
-    let foundBirds = await Bird.find({});
-    res.render("index", { birds: foundBirds });
+    let foundHikes = await Hike.find({});
+    res.render("index", { hikes: foundHikes });
   } catch (err) {
     console.log(err);
   }
 });
 
 // NEW renders a form
-app.get("/birds/new", (req, res) => {
+app.get("/hikes/new", (req, res) => {
   res.render("new");
 });
 
 // CREATE
-app.post("/birds", async (req, res) => {
+app.post("/hikes", async (req, res) => {
   try {
-    let newBird = await Bird.create(req.body.bird);
-    res.redirect("/birds");
+    let newHike = await Hike.create(req.body.hike);
+    res.redirect("/hikes");
   } catch (err) {
     console.log(err);
   }
 });
 
 // SHOW
-app.get("/birds/:id", async(req, res) => {
+app.get("/hikes/:id", async (req, res) => {
   try {
-    let foundBird = await Bird.findById(req.params.id);
-    res.render("show", {bird: foundBird});
-    if (!foundBird) {
-      throw ("OOPS! NO BIRD")
+    let foundHike = await Hike.findById(req.params.id);
+    res.render("show", { hike: foundHike });
+    if (!foundHike) {
+      throw "OOPS! NO HIKE";
     }
-  } catch(err) {
-    console.log(error);
-    res.redirect("/blogs");
+  } catch (err) {
+    console.log(err);
+    res.redirect("/hikes");
   }
-    
-})
-
+});
+// EDIT
+app.get("/hikes/:id/edit", async (req, res) => {
+  try {
+    let foundHike = await Hike.findById(req.params.id);
+    res.render("edit", { hike: foundHike });
+    if (!foundHike) {
+      throw "Something Went WRONG!";
+    }
+  } catch (error) {
+    console.log("Caught", error);
+    res.redirect("/hikes");
+  }
+});
+// Update
+app.put("/hikes/:id", async (req, res) => {
+  try {
+    let updatedHike = await Hike.findByIdAndUpdate(
+      req.params.id,
+      req.body.hike
+    );
+    res.redirect("/hikes/" + req.params.id);
+    if (!updatedHike) {
+      throw "Failed to Update Hike";
+    }
+  } catch (e) {
+    console.log(e);
+    res.redirect("/hikes");
+  }
+});
 app.listen(3000, () => {
   console.log("Server Running on Port 3000");
 });
