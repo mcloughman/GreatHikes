6523cec3e5b40a520c0bd31cae721748fe724b56
mcloughman <51391940+mcloughman@users.noreply.github.com>
2020-10-10 09:05:50 -0700

    Authentication routes and middleware & create and use currentUser

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+
 const methodOverride = require("method-override");
-const axios = require("axios");
+
 const Hike = require("./models/hike");
 const Comment = require("./models/comment");
-//const User = require("./models/user");
+const User = require("./models/user");
 
 // APP CONFIG
-const app = express();
+
 mongoose
   .connect("mongodb://localhost:27017/birdproject", {
     useNewUrlParser: true,
@@ -17,10 +21,30 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 mongoose.set("useFindAndModify", false);
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(methodOverride("_method"));
+
+// PASSPORT INITIALIZATION
+app.use(
+  require("express-session")({
+    secret: "the speed of michael grabner",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 // ROUTES
 // INDEX
@@ -60,7 +84,6 @@ app.get("/hikes/:id", function (req, res) {
       if (err) {
         console.log(err);
       } else {
-        console.log(foundHike);
         res.render("hikes/show", { hike: foundHike });
       }
     });
@@ -98,7 +121,7 @@ app.put("/hikes/:id", async (req, res) => {
 //************************
 // COMMENT ROUTES
 //************************
-app.get("/hikes/:id/comments/new", async (req, res) => {
+app.get("/hikes/:id/comments/new", isLoggedIn, async (req, res) => {
   try {
     let foundHike = await Hike.findById(req.params.id);
     res.render("comments/new", { hike: foundHike });
@@ -110,7 +133,7 @@ app.get("/hikes/:id/comments/new", async (req, res) => {
   }
 });
 
-app.post("/hikes/:id/comments", (req, res) => {
+app.post("/hikes/:id/comments", isLoggedIn, (req, res) => {
   Hike.findById(req.params.id, (err, foundHike) => {
     if (err) {
       console.log(err);
@@ -128,6 +151,53 @@ app.post("/hikes/:id/comments", (req, res) => {
     }
   });
 });
+
+// *****************
+// AUTH ROUTES
+// *****************
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+// handle sign up logic
+app.post("/register", async (req, res) => {
+  let newUser = new User({ username: req.body.username });
+  try {
+    await User.register(newUser, req.body.password);
+    if (!newUser) {
+      throw "ERROR!";
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function () {
+      res.redirect("/hikes");
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/hikes",
+    failureRedirect: "/login",
+  }),
+  (req, res) => {}
+);
+
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/hikes");
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
 
 app.listen(3000, () => {
   console.log("Server Running on Port 3000");
