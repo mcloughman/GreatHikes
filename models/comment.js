0e2f93873c1331f02c46cac83414e6678bb10ab4
mcloughman <51391940+mcloughman@users.noreply.github.com>
2020-10-27 20:57:10 -0700

    add hikeRating to comment schema and update commment views new & edit as well as show page

--- models/comment.js
+++ models/comment.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 const commentSchema = new mongoose.Schema({
   text: String,
   createdAt: { type: Date, default: Date.now },
+  hikeRating: {
+    type: Number,
+    min: 1,
+    max: 5,
+  },
   author: {
     id: {
       type: mongoose.Schema.Types.ObjectId,
